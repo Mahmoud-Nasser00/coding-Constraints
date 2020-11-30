39cb97a826e4f2d4a3c78eb003cefa837ec71abe
Mahmoud-Nasser01 <m.nasser.fulltact@gmail.com>
2020-11-30 12:25:22 +0200

    Initial Commit

---
+++ projects6b constraint Layout by code/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projects6b constraint Layout by code
+//
+//  Created by Mahmoud Nasser on 30/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
